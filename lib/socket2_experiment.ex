dc4a6d28bfadb7cd53c1de121ac5c48f7f59d7eb
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-26 19:20:32 +0900

    Socket.Web.sendサンプル

--- lib/socket2_experiment.ex
+++ lib/socket2_experiment.ex
@@ -12,7 +12,21 @@ defmodule Socket2Experiment do
       :world
 
   """
+
   def hello do
+    phx_join = """
+    ["3","3","room:lobby","phx_join",{}]
+    """
+
+    data = """
+    ["3","4","room:lobby","new_msg",{"body":"テスト Socket.Web.send!"}]
+    """
+
+    socket =
+      Socket.Web.connect!("localhost", 4000, path: "/socket/websocket?token=undefined&vsn=2.0.0")
+
+    Socket.Web.send(socket, {:text, phx_join})
+    Socket.Web.send(socket, {:text, data})
     :world
   end
 end
