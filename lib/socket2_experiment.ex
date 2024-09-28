2c2c51bbdf2debe3c42331c29e092507faa90cdc
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2024-09-28 20:23:43 +0900

    VR連携デモ

--- lib/socket2_experiment.ex
+++ lib/socket2_experiment.ex
@@ -1,4 +1,6 @@
 defmodule Socket2Experiment do
+  alias Socket.Web
+
   @moduledoc """
   Documentation for `Socket2Experiment`.
   """
@@ -6,27 +8,52 @@ defmodule Socket2Experiment do
   @doc """
   Hello world.
 
-  ## Examples
-
-      iex> Socket2Experiment.hello()
-      :world
-
   """
 
   def hello do
+    Web.connect!("localhost", 4000, path: "/socket/websocket?token=undefined&vsn=2.0.0")
+    |> join()
+    |> move("Box", "1.0", "2.0", "1.0")
+    |> move("Cylinder", "1.0", "2.0", "2.0")
+    |> move("Box", "1.0", "3.0", "1")
+    |> move("Box", "1.0", "4.0", "1")
+    |> move("Box", "1.0", "5.0", "1")
+    |> move("Cylinder", "1.0", "2.0", "3")
+    |> move("Cylinder", "1.0", "2.0", "4")
+    |> move("Cylinder", "1.0", "2.0", "5")
+
+    :world
+  end
+
+  def move(socket, name, x, y, z) do
+    data =
+      create_field("move") <>
+        create_field(name) <> create_field(x) <> create_field(y) <> create_field(z)
+
+    send_data(socket, data)
+  end
+
+  def create_field(value) do
+    (value <> String.duplicate(" ", 20))
+    |> String.slice(0, 20)
+  end
+
+  def send_data(socket, data) do
+    data = """
+    ["3","4","room:lobby","new_msg",{"body":"#{data}"}]
+    """
+
+    Web.send!(socket, {:text, data})
+    Process.sleep(250)
+    socket
+  end
+
+  def join(socket) do
     phx_join = """
     ["3","3","room:lobby","phx_join",{}]
     """
 
-    data = """
-    ["3","4","room:lobby","new_msg",{"body":"テスト Socket.Web.send!"}]
-    """
-
-    socket =
-      Socket.Web.connect!("localhost", 4000, path: "/socket/websocket?token=undefined&vsn=2.0.0")
-
-    Socket.Web.send(socket, {:text, phx_join})
-    Socket.Web.send(socket, {:text, data})
-    :world
+    Web.send!(socket, {:text, phx_join})
+    socket
   end
 end
